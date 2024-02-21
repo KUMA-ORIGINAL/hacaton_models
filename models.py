9fdc5392e7ae66f6f9cda39e88fc62c2b0291094
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2024-02-22 00:04:54 +0600

    add last models

--- models.py
+++ models.py
@@ -37,9 +37,9 @@ class User(Base):
     phone_number: Mapped[Optional[str]] = mapped_column(String(200), unique=True)
     email: Mapped[Optional[str]] = mapped_column(String(200), unique=True)
 
-    client: Mapped['Client'] = relationship(back_populates='user', lazy='selection')
+    client: Mapped['Client'] = relationship(back_populates='user')
     employees: Mapped[List['Employee']] = relationship(
-        secondary=users_employees, back_populates='users', lazy='selection'
+        secondary=users_employees, back_populates='users'
     )
 
 
@@ -50,7 +50,7 @@ class Role(Base):
     name: Mapped[str] = mapped_column(String(200))
     permissions: Mapped[PermissionEnum] = mapped_column(Enum(PermissionEnum))
 
-    employees: Mapped[List['Employee']] = relationship(back_populates='role', lazy='selection')
+    employees: Mapped[List['Employee']] = relationship(back_populates='role')
 
 
 class Client(User):
@@ -64,7 +64,7 @@ class Client(User):
     birth_date: Mapped[datetime.date] = mapped_column(DATE)
 
     user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
-    user: Mapped['User'] = relationship(back_populates='client', lazy='selection')
+    user: Mapped['User'] = relationship(back_populates='client')
 
     __table_args__ = (UniqueConstraint("user_id"),)
 
@@ -81,11 +81,71 @@ class Employee(User):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     role_id: Mapped[int] = mapped_column(ForeignKey('roles.id'))
-    role: Mapped[Role] = relationship(back_populates='employees', lazy='selection')
+    role: Mapped[Role] = relationship(back_populates='employees')
     users: Mapped[List['User']] = relationship(
-        secondary=users_employees, back_populates='employees', lazy='selection'
+        secondary=users_employees, back_populates='employees'
     )
-    #
+    organization_id: Mapped[int] = mapped_column(ForeignKey('organizations.id'))
+    organization: Mapped['Organization'] = relationship(back_populates='employees')
+
     __mapper_args__ = {
         'inherit_condition': id == User.id
     }
+
+
+organizations_services = Table(
+    'organizations_services',
+    Base.metadata,
+    Column('organizations_id', ForeignKey('organizations.id'), primary_key=True),
+    Column('services_id', ForeignKey('services.id'), primary_key=True)
+)
+
+services_categories = Table(
+    'services_categories',
+    Base.metadata,
+    Column('services_id', ForeignKey('services.id'), primary_key=True),
+    Column('category_id', ForeignKey('categories.id'), primary_key=True)
+)
+
+
+class Organization(Base):
+    __tablename__ = 'organizations'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(200))
+    address: Mapped[Optional[str]] = mapped_column(String(200))
+
+    employees: Mapped[List['Employee']] = relationship(back_populates='organization')
+    services: Mapped[List['Service']] = relationship(
+        secondary=organizations_services, back_populates='organizations'
+    )
+
+
+class Service(Base):
+    __tablename__ = 'services'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(200))
+    description: Mapped[str]
+
+    organizations: Mapped[List['Organization']] = relationship(
+        secondary=organizations_services, back_populates='services'
+    )
+    categories: Mapped[List['Category']] = relationship(
+        secondary=services_categories, back_populates='services'
+    )
+
+
+class Category(Base):
+    __tablename__ = 'categories'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(200))
+
+    parent_id: Mapped[int] = relationship(ForeignKey('categories.id'))
+    parent: Mapped['Category'] = relationship(
+        'Category', remote_side=[id], backref='subcategories'
+    )
+    services: Mapped[List['Service']] = relationship(
+        secondary=services_categories, back_populates='categories'
+    )
